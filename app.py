f577799c73702f9ba367d7ed4f4b5f47d76c421b
online1004 <117432792+online1004@users.noreply.github.com>
2023-01-16 15:32:11 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,48 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+import time, os, requests
+
+def CheckURL(userid):
+        result = requests.get(f"https://pushoong.com/ask/{userid}")
+
+        if result.status_code == 200:
+            return True
+        else:
+            return False
+
+def SpamPushoong(userid, msg, thread):
+    options = Options()
+    options.add_experimental_option("detach", True)
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service, options=options)
+
+    url = f'https://pushoong.com/ask/{userid}'
+    driver.get(url)
+    time.sleep(0.5)
+    driver.find_element(By.ID, "hide-modal-long").click()
+    time.sleep(0.5)
+    for i in range(thread):
+        driver.find_element(By.ID, "ask_textarea").send_keys(msg)
+        time.sleep(0.5)
+        driver.find_element(By.ID, "ask_send").click()
+        time.sleep(0.5)
+    driver.quit()
+    print("[+] SUCCESSㅣ도배가 성공적으로 완료되었습니다.")
+
+def main():
+    os.system("color B")
+    print('=====================================')
+    ids = input("아이디를 입력하세요 : ")
+    thread = int(input("횟수를 입력하세요 : "))
+    msg = input("메시지를 입력하세요 : ")
+    print('=====================================')
+    
+    if CheckURL(ids):
+        SpamPushoong(ids, msg, thread)
+    else:
+        print('존재하지 않는 ID 입니다.')
+
+main()
